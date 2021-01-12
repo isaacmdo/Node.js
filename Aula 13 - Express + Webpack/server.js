34b7f82ab950ca5dac2aa96beafa10927c273290
Isaac Moura de Oliveira <isaacestudo1999@gmail.com>
2021-01-12 10:05:32 -0500

    ...

---
+++ Aula 13 - Express + Webpack/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { allowedNodeEnvironmentFlags } = require('process');
+const app = express();
+const routes = require('./routes')
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));//<< caminho absoluto da pasta public para conteudos staticos
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));//<< aqui está o caminho absoluto da pasta view
+app.set('view engine', 'ejs'); //<< setamos essa engina para podermos usar codigo de programacao (for, if, echo...) dentro do html e podermos renderizar a view
+//npm i ejs para instalar a dependencia
+
+app.use(routes);
+
+app.listen(3000), () =>{
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor rodando...');
+};
